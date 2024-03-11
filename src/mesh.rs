99cd4326421916ed56510e581fb4482c9653bd27
Quaternions <krakow20@gmail.com>
2024-03-10 21:37:38 -0700

    parser for v2.00

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,6 +1,8 @@
 //based on https://github.com/MaximumADHD/Rbx2Source/blob/main/Geometry/Mesh.cs
 use std::io::{Read,BufRead};
 
+use binrw::BinReaderExt;
+
 #[derive(Debug)]
 pub enum Error{
 	Io(std::io::Error),
@@ -16,11 +18,13 @@ pub enum Error{
 	TextureCoordsDimensionNot3(usize),
 	VertexTripletCount,
 	VertexCount,
+	//2.00
+	BinRead(binrw::Error),
 }
 
 pub enum VersionedMesh{
 	Version1(Mesh1),
-	//Version2(Mesh2),
+	Version2(Mesh2),
 	//Version3(Mesh3),
 	//Version4(Mesh4),
 	//Version5(Mesh5),
@@ -34,7 +38,7 @@ pub fn read<R:Read>(mut read:R)->Result<VersionedMesh,Error>{
 	match &buf{
 		b"version 1.00"=>Ok(VersionedMesh::Version1(read_100(read)?)),
 		b"version 1.01"=>Ok(VersionedMesh::Version1(read_101(read)?)),
-		//b"version 2.00"=>Ok(VersionedMesh::Version2(read_200(read)?)),
+		b"version 2.00"=>Ok(VersionedMesh::Version2(read_200(read)?)),
 		//b"version 3.00"=>Ok(VersionedMesh::Version3(read_300(read)?)),
 		//b"version 3.01"=>Ok(VersionedMesh::Version3(read_301(read)?)),
 		//b"version 4.00"=>Ok(VersionedMesh::Version4(read_400(read)?)),
@@ -144,3 +148,102 @@ pub fn read1<R:Read>(read:R)->Result<Mesh1,Error>{
 	}).collect::<Result<Vec<Vertex1>,Error>>()?})
 }
 
+#[binrw::binrw]
+#[brw(little)]
+pub struct Header2{
+	#[brw(magic=b"\x0C\0\x28\x0C")]//12u16,40u8,12u8
+	//sizeof_header:u16,//size of this struct...
+	//sizeof_vertex:u8,
+	//sizeof_face:u8,
+	pub num_verts:u32,
+	pub num_faces:u32,
+}
+#[binrw::binrw]
+#[brw(little)]
+pub struct Vertex2{
+	pub pos:[f32;3],
+	pub norm:[f32;3],
+	pub tex:[f32;2],
+	pub tangent:[i8;4],// Tangent Vector & Bi-Normal Direction
+	pub color:[u8;4],
+}
+#[binrw::binrw]
+#[brw(little)]
+pub struct Face2(pub u32,pub u32,pub u32);
+#[binrw::binrw]
+#[brw(little)]
+pub struct Mesh2{
+	#[brw(magic=b'\n')]//probably not the greatest idea but whatever
+	pub header:Header2,
+	#[br(count=header.num_verts)]
+	pub vertices:Vec<Vertex2>,
+	#[br(count=header.num_faces)]
+	pub faces:Vec<Face2>,
+}
+
+//alternate version with truncated vertex...
+#[binrw::binrw]
+#[brw(little)]
+struct Header2_36{
+	#[brw(magic=b"\x0C\0\x24\x0C")]//12u16,36u8,12u8
+	//sizeof_header:u16,//size of this struct...
+	//sizeof_vertex:u8,
+	//sizeof_face:u8,
+	num_verts:u32,
+	num_faces:u32,
+}
+#[binrw::binrw]
+#[brw(little)]
+struct Vertex2_36{
+	pos:[f32;3],
+	norm:[f32;3],
+	tex:[f32;2],
+	tangent:[i8;4],// Tangent Vector & Bi-Normal Direction
+}
+#[binrw::binrw]
+#[brw(little)]
+struct Mesh2_36{
+	#[brw(magic=b'\n')]//probably not the greatest idea but whatever
+	header:Header2_36,
+	#[br(count=header.num_verts)]
+	vertices:Vec<Vertex2_36>,
+	#[br(count=header.num_faces)]
+	faces:Vec<Face2>,
+}
+
+#[inline]
+pub fn read_200<R:Read>(mut read:R)->Result<Mesh2,Error>{
+	//placeholder code lol
+	let mut buf=Vec::new();
+	read.read_to_end(&mut buf).map_err(Error::Io)?;
+	read2(binrw::io::Cursor::new(buf))
+}
+
+pub fn read2<R:BinReaderExt>(mut read:R)->Result<Mesh2,Error>{
+	match read.read_le(){
+		//read normally
+		Ok(mesh)=>Ok(mesh),
+		Err(binrw::Error::BadMagic{..})=>{
+			//read truncated vertex mesh
+			let mesh:Mesh2_36=read.read_le().map_err(Error::BinRead)?;
+			//convert to normal
+			Ok(Mesh2{
+				header:Header2{
+					num_verts:mesh.header.num_verts,
+					num_faces:mesh.header.num_faces,
+				},
+				vertices:mesh.vertices.into_iter().map(|v|{
+					Vertex2{
+						pos:v.pos,
+						norm:v.norm,
+						tex:v.tex,
+						tangent:v.tangent,
+						color:[255u8;4],
+					}
+				}).collect(),
+				faces:mesh.faces,
+			})
+		},
+		Err(other)=>Err(Error::BinRead(other)),
+	}
+}
