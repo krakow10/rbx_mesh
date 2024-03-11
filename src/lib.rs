c8c28410b4b4cb1809f280a621689fb2e97f9b80
Quaternions <krakow20@gmail.com>
2024-03-11 08:38:06 -0700

    move tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,8 @@
 mod mesh;
+#[cfg(test)]
+mod test;
 
 #[inline]
 pub fn read<R:std::io::Read+std::io::Seek>(read:R)->Result<mesh::VersionedMesh,mesh::Error>{
 	mesh::read(read)
-}
-
-#[cfg(test)]
-mod test{
-	use super::*;
-	fn load_mesh(name:&str)->Result<mesh::VersionedMesh,mesh::Error>{
-		read(std::fs::File::open(name).map_err(mesh::Error::Io)?)
-	}
-	#[test]
-	fn mesh_100(){
-		load_mesh("meshes/158071912").unwrap();
-	}
-	#[test]
-	fn mesh_200(){
-		load_mesh("meshes/torso.mesh").unwrap();
-	}
 }
---
+++ src/test.rs
@@ -0,0 +1,12 @@
+use crate::mesh::*;
+fn load_mesh(name:&str)->Result<VersionedMesh,Error>{
+	read(std::fs::File::open(name).map_err(Error::Io)?)
+}
+#[test]
+fn mesh_100(){
+	load_mesh("meshes/158071912").unwrap();
+}
+#[test]
+fn mesh_200(){
+	load_mesh("meshes/torso.mesh").unwrap();
+}
