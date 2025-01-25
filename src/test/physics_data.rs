270c9ce26a9d1ef4a004e261b31802d7f1e7cb87
Quaternions <krakow20@gmail.com>
2025-01-25 05:06:26 -0800

    separate tests

---
+++ src/test/physics_data.rs
@@ -0,0 +1,17 @@
+fn read_physics_data(data:&[u8]){
+	let mut cursor=std::io::Cursor::new(data);
+	crate::read_physics_data(&mut cursor).unwrap();
+	assert_eq!(cursor.position(),data.len() as u64);
+}
+#[test]
+fn csgphs_3(){
+	read_physics_data(include_bytes!("../../meshes/CSGPHS_3.data"));
+}
+#[test]
+fn csgphs_5(){
+	read_physics_data(include_bytes!("../../meshes/CSGPHS_5.data"));
+}
+#[test]
+fn csgk(){
+	read_physics_data(include_bytes!("../../meshes/CSGK.data"));
+}
