ea85a16896ecd2078480e6454cc7d4732fe2268c
Quaternions <krakow20@gmail.com>
2025-01-28 13:15:57 -0800

    wip: CSGMDL5

--- src/mesh_data.rs
+++ src/mesh_data.rs
@@ -224,6 +224,65 @@ pub struct CSGMDL5{
 	// pub rest:Vec<u8>,
 }
 #[binrw::binrw]
+#[brw(little,repr=u8)]
+#[derive(Debug,Clone)]
+// Why does this differ from Roblox's own standard?
+pub enum NormalId5{
+	Right=1,
+	Top=2,
+	Back=3,
+	Left=4,
+	Bottom=5,
+	Front=6,
+}
+#[binrw::binrw]
+#[brw(little)]
+#[derive(Debug,Clone)]
+pub struct MeshData5{
+	// #[brw(magic=b"CSGMDL\x05\0\0\0")] but obfuscated
+	#[brw(magic=b"\x15\x7d\x29\x15\x75\x6c\x35\x04\x34\x69")]
+	pub pos_count:u16,//208
+	#[br(count=pos_count)]
+	pub pos:Vec<[f32;3]>,
+
+	// probably has to do with normals
+	pub _unknown1_count:u16,//208
+	pub _unknown1_len:u32,//208*6 = 1248
+	#[br(count=_unknown1_count)]
+	pub _unknown1_list:Vec<[u8;6]>,// 1248 bytes long
+
+	pub color_count:u16,//208
+	#[br(count=color_count)]
+	pub colors:Vec<[u8;4]>,
+
+	pub normal_id_count:u16,//208
+	#[br(count=normal_id_count)]
+	pub normal_id_list:Vec<NormalId5>,
+
+	pub tex_count:u16,//208
+	#[br(count=tex_count)]
+	pub tex:Vec<[f32;2]>,
+
+	// probably has to do with tangents
+	pub _unknown4_count:u16,//208
+	pub _unknown4_len:u32,//208*6 = 1248
+	#[br(count=_unknown4_count)]
+	pub _unknown4_list:Vec<[u8;6]>,// 1248 bytes long
+
+	// triangle strip? u8 because each one is an increment on previous ids?
+	pub _unknown5_count1:u32,//984
+	pub _unknown5_count2:u32,//986
+	#[br(count=_unknown5_count2)]
+	pub _unknown5_list:Vec<u8>,
+
+	pub _unknown6_count:u8,//2-3
+	#[br(count=_unknown6_count)]
+	// the numbers in this list seem to match various list lengths
+	pub _unknown6_list:Vec<u32>,
+	// #[br(parse_with=binrw::helpers::until_eof)]
+	// pub rest:Vec<u8>,
+}
+#[binrw::binrw]
 #[brw(little)]
 #[derive(Debug,Clone)]
 pub enum CSGMDL{
