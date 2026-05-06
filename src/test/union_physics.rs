72aee7877a0d27470fd63a59bedf96fc04e94ee5
Rhys Lloyd <krakow20@gmail.com>
2026-05-06 10:58:36 -0700

    rename modules

--- src/test/union_physics.rs
+++ src/test/union_physics.rs
@@ -1,22 +1,22 @@
-fn read_physics_data(data: &[u8]) {
+fn read_union_physics(data: &[u8]) {
 	let mut cursor = std::io::Cursor::new(data);
-	crate::read_physics_data_versioned(&mut cursor).unwrap();
+	crate::read_union_physics_versioned(&mut cursor).unwrap();
 	assert_eq!(cursor.position(), data.len() as u64);
 }
 
 #[test]
 fn csgphs_3() {
-	read_physics_data(include_bytes!("../../meshes/CSGPHS_3.data"));
+	read_union_physics(include_bytes!("../../meshes/CSGPHS_3.data"));
 }
 #[test]
 fn csgphs_5() {
-	read_physics_data(include_bytes!("../../meshes/CSGPHS_5.data"));
+	read_union_physics(include_bytes!("../../meshes/CSGPHS_5.data"));
 }
 #[test]
 fn csgphs_7() {
-	read_physics_data(include_bytes!("../../meshes/CSGPHS_7.data"));
+	read_union_physics(include_bytes!("../../meshes/CSGPHS_7.data"));
 }
 #[test]
 fn csgk() {
-	read_physics_data(include_bytes!("../../meshes/CSGK.data"));
+	read_union_physics(include_bytes!("../../meshes/CSGK.data"));
 }
