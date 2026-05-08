25e920efe1e91f9030dafc3c0ba865244924d851
Rhys Lloyd <krakow20@gmail.com>
2026-05-08 12:21:33 -0700

    put block in its own file

--- src/union_physics/mod.rs
+++ src/union_physics/mod.rs
@@ -1,3 +1,5 @@
+mod block;
+pub use block::*;
 mod v3;
 pub use v3::*;
 mod v5;
@@ -15,14 +17,6 @@ pub type Error = binrw::Error;
 pub fn read_versioned<R: binrw::BinReaderExt>(mut read: R) -> Result<UnionPhysics, Error> {
 	read.read_le()
 }
-
-/// This mesh is a rectangular prism, also known as a block.
-#[binrw::binrw]
-#[brw(little)]
-// concat_bytes!(b"CSGPHS",0u32,b"BLOCK")
-#[brw(magic = b"CSGPHS\0\0\0\0BLOCK")]
-#[derive(Debug, Clone)]
-pub struct Block;
 
 #[binrw::binrw]
 #[brw(little)]
