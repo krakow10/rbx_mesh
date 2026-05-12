e05e65c8629317519f03395df2f251739539feaf
Rhys Lloyd <krakow20@gmail.com>
2026-05-12 10:58:43 -0700

    move csgk test

--- src/test/union_physics.rs
+++ src/test/union_physics.rs
@@ -3,6 +3,12 @@ use crate::union_physics::{CSGK, CSGPHS3, CSGPHS5, CSGPHS7, CSGPHS8};
 use std::fs::read;
 
 #[test]
+fn csgk() {
+	let bytes = read("meshes/CSGK.data").unwrap();
+	let mesh = roundtrip::<CSGK>(bytes).unwrap();
+	insta::assert_debug_snapshot!(mesh);
+}
+#[test]
 fn csgphs_3() {
 	let bytes = read("meshes/CSGPHS_3.data").unwrap();
 	let mesh = roundtrip::<CSGPHS3>(bytes).unwrap();
@@ -25,11 +31,5 @@ fn csgphs_7() {
 fn csgphs_8() {
 	let bytes = read("meshes/CSGPHS_8_00.data").unwrap();
 	let mesh = roundtrip::<CSGPHS8>(bytes).unwrap();
-	insta::assert_debug_snapshot!(mesh);
-}
-#[test]
-fn csgk() {
-	let bytes = read("meshes/CSGK.data").unwrap();
-	let mesh = roundtrip::<CSGK>(bytes).unwrap();
 	insta::assert_debug_snapshot!(mesh);
 }
