30c8f52903d746b02a9409edbe7d9904f4de6bb3
Rhys Lloyd <krakow20@gmail.com>
2026-05-27 10:46:14 -0700

    discover args_iter

--- src/draco/attributes.rs
+++ src/draco/attributes.rs
@@ -168,46 +168,48 @@ pub struct PredictionData {
 // void SequentialIntegerAttributeDecoder_DecodeIntegerValues() {
 #[binrw::binrw]
 #[brw(little)]
+#[br(import_raw(metadata:&AttributeDecoderMetadata))]
 #[derive(Debug, Clone)]
 pub struct SequentialIntegerAttributeDecoderDecodeIntegerValues {
-	pub prediction_scheme: SeqAttDecPredictionScheme,
-	#[br(if(prediction_scheme != SeqAttDecPredictionScheme::PredictionNone))]
-	pub ext: Option<PredictionDataExt>,
+	// num_components = GetNumComponents();
+	// num_entries = att_dec_num_values_to_decode[curr_att_dec][curr_att];
+	// num_values = num_entries * num_components;
+	// if (seq_int_att_dec_compressed[curr_att_dec][curr_att] > 0) {
+	//   DecodeSymbols(num_values, num_components, &decoded_symbols);
+	// }
+	// seq_int_att_dec_decoded_values[curr_att_dec][curr_att] = decoded_symbols;
+	// if (num_values > 0) {
+	//   if (seq_att_dec_prediction_transform_type[curr_att_dec][curr_att] ==
+	//         PREDICTION_TRANSFORM_NORMAL_OCTAHEDRON_CANONICALIZED) {
+	//     decoded_symbols = seq_int_att_dec_decoded_values[curr_att_dec][curr_att];
+	//     for (i = 0; i < decoded_symbols.size(); ++i) {
+	//       signed_vals[i] = decoded_symbols[i];
+	//     }
+	//     seq_int_att_dec_symbols_to_signed_ints[curr_att_dec][curr_att] = signed_vals;
+	//   } else {
+	//     ConvertSymbolsToSignedInts();
+	//   }
+	// }
+	// if (seq_att_dec_prediction_scheme[curr_att_dec][curr_att] != PREDICTION_NONE) {
+	//   DecodePredictionData(seq_att_dec_prediction_scheme[curr_att_dec][curr_att]);
+	//   PredictionScheme_ComputeOriginalValues(
+	//       seq_att_dec_prediction_scheme[curr_att_dec][curr_att], num_entries);
+	// }
 }
-//   num_components = GetNumComponents();
-//   num_entries = att_dec_num_values_to_decode[curr_att_dec][curr_att];
-//   num_values = num_entries * num_components;
-//   if (seq_int_att_dec_compressed[curr_att_dec][curr_att] > 0) {
-//     DecodeSymbols(num_values, num_components, &decoded_symbols);
-//   }
-//   seq_int_att_dec_decoded_values[curr_att_dec][curr_att] = decoded_symbols;
-//   if (num_values > 0) {
-//     if (seq_att_dec_prediction_transform_type[curr_att_dec][curr_att] ==
-//           PREDICTION_TRANSFORM_NORMAL_OCTAHEDRON_CANONICALIZED) {
-//       decoded_symbols = seq_int_att_dec_decoded_values[curr_att_dec][curr_att];
-//       for (i = 0; i < decoded_symbols.size(); ++i) {
-//         signed_vals[i] = decoded_symbols[i];
-//       }
-//       seq_int_att_dec_symbols_to_signed_ints[curr_att_dec][curr_att] = signed_vals;
-//     } else {
-//       ConvertSymbolsToSignedInts();
-//     }
-//   }
-//   if (seq_att_dec_prediction_scheme[curr_att_dec][curr_att] != PREDICTION_NONE) {
-//     DecodePredictionData(seq_att_dec_prediction_scheme[curr_att_dec][curr_att]);
-//     PredictionScheme_ComputeOriginalValues(
-//         seq_att_dec_prediction_scheme[curr_att_dec][curr_att], num_entries);
-//   }
-// }
 
 #[binrw::binrw]
 #[brw(little)]
 #[derive(Debug, Clone)]
+#[br(import_raw(metadata:&AttributeDecoderMetadata))]
 pub struct Attribute {
+	// DecodePortableAttributes();
 	pub prediction_data: PredictionData,
+	#[br(args_raw(metadata))]
 	#[br(if(prediction_data.prediction_scheme != SeqAttDecPredictionScheme::PredictionNone))]
 	pub sequential_integer_attribute_decoder_decode_integer_values:
 		Option<SequentialIntegerAttributeDecoderDecodeIntegerValues>,
+	// DecodeDataNeededByPortableTransforms();
+	// TransformAttributesToOriginalFormat();
 }
 
 #[binrw::binrw]
@@ -271,4 +273,6 @@ pub struct Attributes {
 	//    TransformAttributesToOriginalFormat();
 	//  }
 	// }
+	#[br(parse_with = binrw::helpers::args_iter(&atribute_decoder_metadatas))]
+	pub portable_attributes: Vec<Attribute>,
 }
